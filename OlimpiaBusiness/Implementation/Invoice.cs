205d65f150de713378add21553c5dafbe7897f00
Juan Buitrago <jcbuitrago@emergiacc.com>
2020-08-13 22:40:11 -0500

    Eliminado referencias que no se utilizan en el proyecto

--- OlimpiaBusiness/Implementation/Invoice.cs
+++ OlimpiaBusiness/Implementation/Invoice.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 using System.Data;
 using System.Text.RegularExpressions;
 
