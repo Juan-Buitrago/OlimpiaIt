a8335de39229c4f067fa7df2e0559e6ea95f1f49
Juan Buitrago <jcbuitrago@emergiacc.com>
2020-08-16 10:04:21 -0500

    Añadido nuevo modulo de consola para el ejercicio 1

---
+++ Application/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Console.WriteLine("Test OlimpiaIt");
+            //List<Mammal> mammals = Bussines.GetFileContentByPath(pathDataFile);
+            //Bussines.FilterAndWriteFiles(mammals, pathFile);
+            //System.Console.WriteLine("Finalised Olimpia Test");
+        }
+    }
+}
